2129cce1cf58490941dccb5f6e16d587e2f325ff
Happy Guleria <happyguleria123@gmail.com>
2021-01-05 17:07:48 +0530

    Initial Commit

---
+++ Pregnancy risk checker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pregnancy risk checker
+//
+//  Created by Apple on 05/01/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pregnancy risk checkerTests/Pregnancy_risk_checkerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pregnancy_risk_checkerTests.swift
+//  Pregnancy risk checkerTests
+//
+//  Created by Apple on 05/01/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import XCTest
+@testable import Pregnancy_risk_checker
+
+class Pregnancy_risk_checkerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
